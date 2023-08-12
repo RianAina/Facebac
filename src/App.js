42558c6d6e1281b3f9fe3b79fd81ae26a0136062
RianAina <hei.rianaina@gmail.com>
2023-08-12 13:52:27 +0300

    feat : initialize the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Ici, vous pouvez mettre votre logique de connexion
+    // par exemple, vérification des informations d'identification
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    
+    <div className="login-container">
+
+<div className='LeftSection'>
+        <h1 className='facebac'>Facebac</h1>
+      <h4 className='facebacText'>Naviguer en toute sereniter avec nous et decouvrer ce qui se passe dans le monde maintenant !</h4>
+      <div className='signature'>ReactProject @WEB2</div>
+      </div>
+      
+      <div className="login-box">
+        <h2>Se connecter</h2>
+        <input
+          type="text"
+          placeholder="Adresse e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Mot de passe"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>Connexion</button>
+        <br></br>
+        <button className='SignUpButton' onClick={handleLogin}>Sign up</button>
+        <br></br>
+        <div className='message'>Créer une Page pour une célébrité, une marque ou une entreprise. </div>
+      </div>
+      
+      
     </div>
   );
 }
